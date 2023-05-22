b285045d61c8c344921f21cbee2acd911632ba80
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-22 17:56:15 +0900

    FEAT: App.tsx 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import Input from "./components/Input";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState('');
+    const changeValue = (e:any) => {
+        console.log(value);
+        setValue(e.target.value);
+    }
+
+    return (
+    <>
+        <Input onChange={changeValue}/>
+    </>
+    );
 }
 
 export default App;
