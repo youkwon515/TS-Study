e7d1f21c6bfda9b0061d88f67722c0bd1387e037
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-23 12:36:59 +0900

    FEAT: Todo 생성

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,4 +1,9 @@
-function Todo() {
+interface TodoProps {
+    deleteTodo: () => void; // 타입을 함수로
+    handleSelected: (checked: boolean) => void; // 매개변수 type이 boolean
+}
+
+function Todo({deleteTodo, handleSelected}:TodoProps) {
     return <div></div>
 }
 
