a2ee60ba91f04f6c263132ab3cb42cd2e5b1f8a0
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-23 15:13:16 +0900

    FEAT: 추가 기능 생성

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,16 +1,44 @@
 import type { DispatchSetStateAction } from '../../types/util';
-import type {Todo} from '../../types/todo'
+import type { Todo as TodoType } from '../../types/todo'
+import Todo from '../Todo';
 interface TodoListProps {
-    todos: Todo[];
+    todos: TodoType[];
     searchValue:string;
-    deleteTodo: (id:Todo['id']) => void;
+    deleteTodo: (id:TodoType['id']) => void;
     // () => 그냥 함수 / 따로 작성한 함수    
     // DispatchSetStateAction = useState의 두번째 인자로 받은 함수
-    setSelectedTodoIds: DispatchSetStateAction<Todo['id'][]>;
+    setSelectedTodoIds: DispatchSetStateAction<TodoType['id'][]>;
 }
 
 function TodoList({todos, searchValue, deleteTodo, setSelectedTodoIds}:TodoListProps) {
-    return <ul></ul>
+    return (
+        <ul>
+            {todos.map(({id, name}) => {
+                const handleDeleteTodo = () => {
+                    deleteTodo(id);
+                };
+
+                const handleSelected = (checked: boolean) => {
+                    if (checked) {
+                        setSelectedTodoIds((prevState) => [...prevState, id]);
+                    } else {
+                        setSelectedTodoIds((prevState) => 
+                            prevState.filter((prevId) => prevId !== id)
+                        );
+                    }
+                };
+
+                return (
+                    <Todo 
+                        key={id} 
+                        name={name}
+                        deleteTodo={handleDeleteTodo} 
+                        handleSelected={handleSelected}
+                    />
+                );
+            })}
+        </ul>
+    )
 }
 
 export default TodoList
