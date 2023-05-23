a419f69476d0e0e57cb42c3cc241e6da9ee5c75e
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-23 15:12:11 +0900

    FEAT: Todo에 li name 추가

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,10 +1,13 @@
+import {Todo as TodoType} from '../../types/todo'
+
 interface TodoProps {
+    name: TodoType['name'];
     deleteTodo: () => void; // 타입을 함수로
     handleSelected: (checked: boolean) => void; // 매개변수 type이 boolean
 }
 
-function Todo({deleteTodo, handleSelected}:TodoProps) {
-    return <div></div>
+function Todo({name, deleteTodo, handleSelected}:TodoProps) {
+    return <li>{name}</li>
 }
 
 export default Todo;
