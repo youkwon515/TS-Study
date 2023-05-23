a476e074b9b5c8ffcee0545ddffd2e2739b0dccb
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-23 09:58:25 +0900

    FEAT: App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { useState } from "react";
 import Input from "./components/Input";
 function App() {
-    const [value, setValue] = useState('');
-    const changeValue = (e:any) => {
-        console.log(value);
-        setValue(e.target.value);
-    }
-
     return (
-    <>
-        <Input onChange={changeValue}/>
-    </>
+        <Input />
     );
 }
 
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,17 +1,47 @@
 import {InputHTMLAttributes} from 'react';
-type MyType = {color: string; value: number;};
+// type 과 interface 선언 차이
+type Color = string;
+type ColorObj = {color: Color};
+
+
+const MyColor1: Color = 'red';
+const MyColor2: ColorObj = {color: "red"};
+
 interface MyInterface {
     color: string;
-    value: number;
-
+    id: number;
 }
 
-const MyData = {
-    color:'#000',
+// type 과 interface 상속 차이
+type MyType2 = {color: string}
+type OpacityData = { opacity: number }
+type MyMixType = MyType2 | OpacityData; // MyType 또는 OpacityData
+const Data: MyMixType = {color: 'red', opacity: 1}
 
+interface Human {
+    name: string;
+    old: number;
 }
 
-function Input(props:InputHTMLAttributes<HTMLInputElement>) {
+interface HJ extends Human {
+    job: string;
+}
+
+const hj: HJ = {name: 'hj', old:23, job:'developer'}
+
+
+interface MyOpacity {
+    opacity: number;
+}
+interface MyInterface2 extends MyOpacity{
+    color: string;
+}
+const Data2: MyInterface2 = {color: 'red', opacity: 1}
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    color?: string; // | undefined
+}
+function Input(props:InputProps) {
     return <input {...props}/>
 }
 
