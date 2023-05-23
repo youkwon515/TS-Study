29b5ac2dbcd6a28da28eb30c92a841af43482777
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-23 12:36:22 +0900

    FEAT: App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import type { DispatchSetStateAction } from './types/util';
+import { Todo } from './types/todo';
+import {useState} from 'react';
 import Input from "./components/Input";
+import TodoList from "./components/TodoList";
 function App() {
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const [searchValue, setSearchValue] = useState<string>('');
+    const [selectedTodoIds, setSelectedTodoIds] = useState<Todo['id'][]>([]);
+    const handleDeleteTodo = (id:Todo['id']) => {
+        setTodos(prevState => {
+            const filterTodos = [...prevState].filter(todo => todo.id !== id);
+            return filterTodos;
+        })
+    }
     return (
-        <Input />
+        <>
+            <Input />
+            <TodoList todos={todos} searchValue={searchValue} deleteTodo={handleDeleteTodo} setSelectedTodoIds={setSelectedTodoIds}/>
+        </>
+        
     );
 }
 
