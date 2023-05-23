6151305faddc0ed9ca175f0d76d29d73abd9c4de
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-23 12:57:28 +0900

    FEAT: handleTodoName 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import type { DispatchSetStateAction } from './types/util';
 import { Todo } from './types/todo';
-import {useState} from 'react';
+import {ChangeEvent, useState} from 'react';
 import Input from "./components/Input";
 import TodoList from "./components/TodoList";
 function App() {
+    const [todoName, setTodoName] = useState<Todo['name']>("");
     const [todos, setTodos] = useState<Todo[]>([]);
     const [searchValue, setSearchValue] = useState<string>('');
     const [selectedTodoIds, setSelectedTodoIds] = useState<Todo['id'][]>([]);
+
+    const handleTodoName = (e: ChangeEvent<HTMLInputElement>) => {
+        setTodoName(e.target.value);
+    }
+
+
     const handleDeleteTodo = (id:Todo['id']) => {
         setTodos(prevState => {
             const filterTodos = [...prevState].filter(todo => todo.id !== id);
@@ -15,7 +22,9 @@ function App() {
     }
     return (
         <>
+            <Input onChange={handleTodoName} />
             <Input />
+            {todoName}
             <TodoList todos={todos} searchValue={searchValue} deleteTodo={handleDeleteTodo} setSelectedTodoIds={setSelectedTodoIds}/>
         </>
         
