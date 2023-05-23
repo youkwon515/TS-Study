be6f14bc715c815870e834d247778b4af678a57a
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-23 15:11:35 +0900

    FEAT: App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,20 @@ import { Todo } from './types/todo';
 import {ChangeEvent, useState} from 'react';
 import Input from "./components/Input";
 import TodoList from "./components/TodoList";
+import {v4 as uuidv4} from 'uuid';
 function App() {
     const [todoName, setTodoName] = useState<Todo['name']>("");
     const [todos, setTodos] = useState<Todo[]>([]);
     const [searchValue, setSearchValue] = useState<string>('');
     const [selectedTodoIds, setSelectedTodoIds] = useState<Todo['id'][]>([]);
 
-    const handleTodoName = (e: ChangeEvent<HTMLInputElement>) => {
-        setTodoName(e.target.value);
+    const handleTodoName = ({target: {value}}: ChangeEvent<HTMLInputElement>) => {
+        setTodoName(value);
     }
+
+    const handleSearchValue = ({target: {value}}: ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(value)
+    } 
 
 
     const handleDeleteTodo = (id:Todo['id']) => {
@@ -20,10 +25,16 @@ function App() {
             return filterTodos;
         })
     }
+
+    const addTodo = () => {
+        setTodos((prevState) => [...prevState, {id: uuidv4(), name: todoName}])
+    }
+
     return (
         <>
             <Input onChange={handleTodoName} />
-            <Input />
+            <button onClick={addTodo}>Todo 추가</button>
+            <Input onChange={handleSearchValue}/>
             {todoName}
             <TodoList todos={todos} searchValue={searchValue} deleteTodo={handleDeleteTodo} setSelectedTodoIds={setSelectedTodoIds}/>
         </>
