9db1c85b20f8e2849b2c160ecb5ed92b11410ac0
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-25 10:32:58 +0900

    FEAT: Todo 수정

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,5 +1,5 @@
 import {Todo as TodoType} from '../../types/todo'
-
+import Button from '../Button';
 interface TodoProps {
     name: TodoType['name'];
     deleteTodo: () => void; // 타입을 함수로
@@ -7,7 +7,12 @@ interface TodoProps {
 }
 
 function Todo({name, deleteTodo, handleSelected}:TodoProps) {
-    return <li>{name}</li>
+    return (
+    <li>
+        <span>{name}</span>
+        <Button onClick={deleteTodo}>삭제</Button>
+    </li> 
+    )
 }
 
 export default Todo;
