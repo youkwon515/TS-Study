60d3548beb63954af1a8feb4c60bef00ce9ee258
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-25 10:32:23 +0900

    FEAT: 삭제 버튼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import type { DispatchSetStateAction } from './types/util';
 import { Todo } from './types/todo';
-import {ChangeEvent, useState} from 'react';
+import {ChangeEvent, useEffect, useState} from 'react';
 import Input from "./components/Input";
 import TodoList from "./components/TodoList";
 import {v4 as uuidv4} from 'uuid';
@@ -27,12 +27,16 @@ function App() {
     }
 
     const addTodo = () => {
-        setTodos((prevState) => [...prevState, {id: uuidv4(), name: todoName}])
+        setTodos((prevState) => [...prevState, {id: uuidv4(), name: todoName}]);
     }
+
+    useEffect(() => {
+        setTodoName('');
+    },[todos])
 
     return (
         <>
-            <Input onChange={handleTodoName} />
+            <Input value={todoName} onChange={handleTodoName} />
             <button onClick={addTodo}>Todo 추가</button>
             <Input onChange={handleSearchValue}/>
             {todoName}
