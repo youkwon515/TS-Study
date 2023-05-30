d98897c5a365732c38de098cf8cf6a97023faaa3
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-31 08:41:45 +0900

    FEAT: liContainer style 추가

---
+++ src/components/Todo/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const liContainer = styled.li`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  > div {
+    display: flex;
+    gap: 5px;
+    align-items: center;
+  }
+`;
