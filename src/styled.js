d0200948579984cce39a969035fbe2b2eea078be
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-31 08:43:12 +0900

    FEAT: InputContainer style 생성

---
+++ src/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  display: flex;
+  gap: 20px;
+  > button {
+    width: 100px;
+    border-radius: 10px;
+    border: 0;
+  }
+`;
