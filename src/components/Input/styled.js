df1412fd54a4c685506937bfa29efb4112ad9678
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-31 08:40:59 +0900

    FEAT: InputBox style 추가

---
+++ src/components/Input/styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const InputBox = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 400px;
+  height: 45px;
+  background-color: #fff;
+  border-radius: 999px;
+  > input {
+    width: 80%;
+    height: 100%;
+    border: 0;
+    outline: none;
+  }
+`;
