edd99de0db81b897f50834fc0ab795fbebfee7ee
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-06-01 12:27:22 +0900

    FEAT: liContainer style 수정

--- src/components/Todo/styled.js
+++ src/components/Todo/styled.js
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 
 export const liContainer = styled.li`
+  background-color: yellow;
   display: flex;
   align-items: center;
+  justify-content: center;
   gap: 20px;
   > div {
     display: flex;
